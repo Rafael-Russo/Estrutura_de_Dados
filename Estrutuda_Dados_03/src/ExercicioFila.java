ae42596576b2b4ffa9043d402c8260258b106fe9
Rafael Russo Soares <12118482@aluno.newtonpaiva.br>
2023-09-15 20:35:21 -0300

    Finaliza o exercicio 01

---
+++ Estrutuda_Dados_03/src/ExercicioFila.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.PriorityQueue;
+
+public class ExercicioFila {
+    Queue<String> fila = new PriorityQueue<>();
+
+
+}
---
+++ Estrutuda_Dados_03/src/ExercicioPilha.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ExercicioPilha {
+    static void exercico01() throws FileNotFoundException {
+        Stack pilha = new Stack();
+        File arq = new File("arquivos\\pesquisas.txt");
+        System.out.println("teste: " + arq.getAbsoluteFile());
+        Scanner ler = new Scanner(arq);
+
+
+        while (ler.hasNextLine()) {
+            pilha.push(ler.nextLine());
+        }
+
+        int i=0;
+
+        while (pilha.size() > 10){
+            if (i<10){
+                System.out.println("top " + (i+1) + ": " + pilha.elementAt(i));
+                i++;
+            }else {
+                pilha.remove(i);
+            }
+        }
+        System.out.println(pilha);
+    }
+}
